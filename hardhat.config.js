4b3971692d3ea0a666bb3c4e469220168917dfa4
macuih <macuihlein2025@u.northwestern.edu>
2025-07-28 19:58:52 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.0", // adjust this to match your contract's pragma
+  solidity: "0.8.0",
   networks: {
     sepolia: {
       url: process.env.ALCHEMY_SEPOLIA_URL,
